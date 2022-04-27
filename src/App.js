f31e18ed64f942175b6a892c7d352b11fc450416
Kim-menu <hyunwjin@gmail.com>
2022-04-27 19:39:00 +0900

    How to use clean up function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import {useEffect, useState} from "react";
 
+function Hello() {
+    useEffect(() => {
+        console.log("Created")
+        return () => console.log("Destroyed");
+    }, []);
+    return <h1>Hello!</h1>;
+}
+
 function App() {
-    const [counter, setCounter] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const increaseCounter = () => {
-        setCounter((curr) => curr + 1);
-    }
-    const onChange = (event) => {
-        setKeyword(event.target.value);
-    }
-    const iRunOnlyOnce = () => (console.log("i run only once"));
-    const searchUpdate = () => (console.log("i searched"));
-    console.log("i run all the time");
-    useEffect(iRunOnlyOnce, []);
-    useEffect(searchUpdate, [keyword]);
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((cur) => !cur);
     return (
         <div>
-            <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-            <h1>{counter}</h1>
-            <button onClick={increaseCounter}>Increase Counter</button>
+            {showing ? <Hello /> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
         </div>
     );
 }
