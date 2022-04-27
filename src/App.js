8842e02ad2292e0297800e8c6e05c9b5409e78d6
Kim-menu <hyunwjin@gmail.com>
2022-04-27 19:24:06 +0900

    Introduce useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div>
-        <h1 className={styles.title}>Hello React World!</h1>
-        <Button text="hey"/>
-    </div>
-  );
+    const [counter, setCounter] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const increaseCounter = () => {
+        setCounter((curr) => curr + 1);
+    }
+    const onChange = (event) => {
+        setKeyword(event.target.value);
+    }
+    const iRunOnlyOnce = () => (console.log("i run only once"));
+    const searchUpdate = () => (console.log("i searched"));
+    console.log("i run all the time");
+    useEffect(iRunOnlyOnce, []);
+    useEffect(searchUpdate, [keyword]);
+    return (
+        <div>
+            <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+            <h1>{counter}</h1>
+            <button onClick={increaseCounter}>Increase Counter</button>
+        </div>
+    );
 }
 
 export default App;
